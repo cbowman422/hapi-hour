7b75729897b6141e077c259fc4fbdcaa4c8535ad
unknown <efraine387@gmail.com>
2022-12-05 15:43:45 -0500

    Setup Routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-
+import React from 'react';
+import Drinks from './components/Drinks';
+import DrinkDetails from './components/DrinkDetails';
+import { Route, Routes } from 'react-router-dom'
 import './App.css';
 
 function App() {
@@ -9,6 +12,12 @@ function App() {
           <a href="/">Cocktails</a>
         </h1>
       </header>
+      <main>
+        <Routes>
+          <Route path='/' element={<Drinks />} />
+          <Route path='/details/:id' element={ <DrinkDetails /> } />
+        </Routes>
+      </main>
     </>
   );
 }
