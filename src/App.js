186784d89f6a322755609013fa55e73d99a08daf
unknown <efraine387@gmail.com>
2022-12-05 15:47:30 -0500

    Setup Route params

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Drinks from './components/Drinks';
 import DrinkDetails from './components/DrinkDetails';
+import Search from './components/Search';
 import { Route, Routes } from 'react-router-dom'
 import './App.css';
 
@@ -14,8 +15,9 @@ function App() {
       </header>
       <main>
         <Routes>
-          <Route path='/' element={<Drinks />} />
-          <Route path='/details/:id' element={ <DrinkDetails /> } />
+          <Route path='/' element={<Search />} />
+          <Route path='/drinks/:id' element={<Drinks />} />
+          <Route path='/drinks/:id/details/:idx' element={ <DrinkDetails /> } />
         </Routes>
       </main>
     </>
