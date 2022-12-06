4e3c033cf046bb6af2bc1c11d5eee69eb3430492
cbowman422 <chrisb0wman@hotmail.com>
2022-12-05 20:51:01 -0500

    accessed api in component

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,32 +5,41 @@ const Search = () => {
     const [search , setSearch] = useState(null)
 
     useEffect(() => {
-        const url = "www.thecocktaildb.com/api/json/v1/1/random.php";
-        fetch(url)// url string
-        .then((response) => response.json())//wait for json response
-        .then((json) => {// then take json and exec
+        const url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin";
+        fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
           setSearch(json)
         })
-        .catch(console.error) // Catch and log any errors to the console
+        .catch(console.error) 
       }, []);
 
-  return (
-    <div className='searchNav'>
-        <input className='searchName' type="text" placeholder="Search..."></input>
-        <div className='filters'>
-            <div className='ingredient'>
-                <input type='radio'></input><span>Gin</span>
-                {/*https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin */}   
-            </div>
-            {search.map((search, idx) => {
-                return (
-                <div key={idx}>{search.strDrink}</div>
-        )
-      })}
 
-        </div>
+  return ( search ?
+
+    <div>
+      {search.drinks.map((searchMap,idx) => {
+        return (
+
+
+      <div key={idx} className='searchNav'>
+        {/* <input className='searchName' type="text" placeholder="Search..."></input>
+        <div className='filters'>
+          <div className='ingredient'>
+          <input type='radio'></input><span>Gin</span>
+          </div> */}
+          <div className="result">
+            {searchMap.strDrink}        
+          </div>
+      
+        {/* </div> */}
+      </div>
+               )
+          })} 
     </div>
-  )
+      :
+    <p> loading .. </p>
+)
 }
 
 export default Search
