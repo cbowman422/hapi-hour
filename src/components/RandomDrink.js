44b14f03ec86761e4cf583e87f17b310ec7b28c0
cbowman422 <chrisb0wman@hotmail.com>
2022-12-09 12:17:17 -0500

    organized randomDrink js

--- src/components/RandomDrink.js
+++ src/components/RandomDrink.js
@@ -2,49 +2,52 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 
-const RandomDrink = () => {
+// Componenent function
+const RandomDrink = () => 
+{
 
-    // State variables    
-    const [refreshPage] = useState(false)
-    const [randomDrink , setRandomDrink] = useState(null)
+// State variables    
+  const [refreshPage] = useState(false)
+  const [randomDrink , setRandomDrink] = useState(null)
 
-    // function that refreshes the state, thus re rendering the useEffect
-    const refreshPageFunction = () => {
-      refreshPage(current => !current)
+// Function that refreshes the state, thus re rendering the useEffect
+  const refreshPageFunction = () => 
+  {
+    refreshPage(current => !current)
       setTimeout(function() {
         refreshPage(current => !current)
-         }, 1);
-    }
-    
-    useEffect(() => {
-        const url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
-        fetch(url)
-        .then((response) => response.json())
-        .then((json) => {
-          setRandomDrink(json)
-        })
-        .catch(console.error) 
-      }, []);
+      }, 1);
+  }
 
-       
-
+// Fetches API and stores it as json in state.
+  useEffect(() => 
+  {
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
+      fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        setRandomDrink(json)
+      })
+      .catch(console.error) 
+  }, []);
+   
+// Conditional return
   return ( randomDrink ?
-
     <div>
-      {randomDrink.drinks.map((randomDrinkMap,randomDrinkIdx) => {
-     return (
-
-    <div className='random' key={randomDrinkIdx}>
-      <Link to={`/drinks-details/${randomDrinkMap.idDrink}`} onClick={refreshPageFunction} > Randomize!
-      </Link>
+      {randomDrink.drinks.map((randomDrinkMap,randomDrinkIdx) => 
+        {
+          return (
+            <div className='random' key={randomDrinkIdx}>
+              <Link to={`/drinks-details/${randomDrinkMap.idDrink}`} onClick={refreshPageFunction} > Randomize!
+              </Link>
+            </div>
+                 )
+        })
+      } 
     </div>
-
-     )
-    })} 
-    </div>
-      :
-    <p>  </p>
-  )
+  :
+    <p> </p>
+        )
 }
 
 export default RandomDrink
