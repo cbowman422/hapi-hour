6360e792a1e9fddf40fd6b720046c375d1e8a358
cbowman422 <chrisb0wman@hotmail.com>
2022-12-09 13:47:50 -0500

    cleaned up app js

--- src/App.js
+++ src/App.js
@@ -9,112 +9,106 @@ import NotFound from './components/NotFound';
 import RandomDrink from './components/RandomDrink';
 import { Link } from 'react-router-dom';
 
-function App() {
+// Component function.
+function App() 
+{
 
-
+// State variables.
   const [searchBarItem, setSearchBarItem] = useState('')
   const [spiritList, setSpiritList] = useState([])
   const [isSearch, setIsSearch] = useState(false)
   const [isSearchIngredient, setIsSearchIngredient] = useState(false)
 
-
-  // Search drink list by name cick function to reset state to false after click
-  const nameClick = () => {
+// Function to change state and reset state to false after click search by name.
+  const nameClick = () => 
+  {
     setIsSearch(current => !current)
-    if (setIsSearch) {
-
-      setTimeout(function() {
+    if (setIsSearch) 
+    {
+      setTimeout(function() 
+      {
         setIsSearch(current => !current)
-         }, 1);
-
-    } else {
-
+      }, 1);
+    } 
+    else 
+    {
     }
   }
 
-
-  // Search drink list by ingredient cick function to reset state to false after click
-  const ingredientClick = () => {
+// Function to change state and reset state to false after click search by ingredient.
+  const ingredientClick = () => 
+  {
     setIsSearchIngredient(current => !current)
-    if (setIsSearchIngredient) {
-
-      setTimeout(function() {
+    if (setIsSearchIngredient) 
+    {
+      setTimeout(function() 
+      {
         setIsSearchIngredient(current => !current)
-         }, 1);
-
-    } else {
-
+      }, 1);
+    } 
+    else 
+    {
     }
   }
 
-
-
-  // Event handler for setting the search bar input to state
-  const handleItemChange = (e) => {
+// Event handler for setting the search bar input to an event and saving that event to state.
+  const handleItemChange = (e) => 
+  {
     const newSearchItem = e.target.value.toUpperCase()
     setSearchBarItem(newSearchItem)
   }
 
 
-  // Grabs the list of ingredients 1 from API
-  useEffect(() => {
+// Grabs the list of ingredients 1 from API for display on home/ search page.
+  useEffect(() => 
+  {
     const url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list";
     fetch(url)
     .then((response) => response.json())
-    .then((json) => {
+    .then((json) => 
+    {
       setSpiritList(json.drinks)
     })
     .catch(console.error) 
   }, []);
  
-
-return ( spiritList ?
-  <div>
-    <header className="header" id="sticky">
+// Conditional return.
+  return ( spiritList ?
+    <div>
+      <header className="header" id="sticky">
         <div className='randomDrinkBtn'>
           <RandomDrink />
         </div>
         <div className='pageTitle'>
           <a href="/search">h-API-Hour</a>
         </div>
-
         <div className="searchBar">
-              <input type="text" id="myInput" value={searchBarItem} onChange={handleItemChange} placeholder='Cocktail name'/>
-             
-             <Link to={`/drinks-details/${searchBarItem}`}> 
-              <button onClick={nameClick}> 
-               search name
-              </button>
-             </Link>
-             
-             <Link to={`/drinks/${searchBarItem}`}> 
-              <button onClick={ingredientClick}> 
-               search ing
-              </button>
-             </Link>
+          <input type="text" id="myInput" value={searchBarItem} onChange={handleItemChange} placeholder='Cocktail name'/>
+            <Link to={`/drinks-details/${searchBarItem}`}> 
+              <button onClick={nameClick}> search name </button>
+            </Link>
+            <Link to={`/drinks/${searchBarItem}`}> 
+              <button onClick={ingredientClick}> search ing </button>
+            </Link>
         </div>
-
-    </header>
-    <main className="container">
-      <div>
-        <Routes>
-          <Route path='/' element={<IngredientOneList spirit={spiritList} visitProp={false}/>} />
-          <Route path='/search/' element={<IngredientOneList spirit={spiritList} visitProp={true}/>} />
-          <Route path='/drinks/:id' element={ <DrinkList ingredientName={searchBarItem} isSearchIngredient={isSearchIngredient} />} />
-
-          {/* <Route path='/drinks-details/' element={ <DrinkDetails drinkName={searchBarItem} isSearch={isSearch}/>} /> */}
-
-          <Route path='/drinks-details/:id' element={ <DrinkDetails drinkName={searchBarItem} isSearch={isSearch} /> } />
-
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </main>
-  </div>
+      </header>
+      <main className="container">
+        <div>
+          <Routes>
+            <Route path='/' element={<IngredientOneList spirit={spiritList} visitProp={false}/>} />
+            <Route path='/search/' element={<IngredientOneList spirit={spiritList} visitProp={true}/>} />
+            <Route path='/drinks/:id' element={ <DrinkList ingredientName={searchBarItem} isSearchIngredient={isSearchIngredient} />} />
+            <Route path='/drinks-details/:id' element={ <DrinkDetails drinkName={searchBarItem} isSearch={isSearch} /> } />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </main>
+    </div>
   :
-  <p> loading .. </p>
-);
+    <p> loading .. </p>
+        );
 }
+
 export default App;
 
 
