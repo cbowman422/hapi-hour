a877426a4ebde7f2863a9f84c8576c643e6ee36b
cbowman422 <chrisb0wman@hotmail.com>
2022-12-09 17:33:41 -0500

    fixed not found js better

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import "../css/NotFound.css"
 
 // Componenent function.
 const NotFound = () => 
@@ -28,12 +29,13 @@ const NotFound = () =>
       {notFound.drinks.map((notFoundMap,notFoundIdx) => 
         {
           return (
-            <div className='container' key={notFoundIdx}>
+            <div className='containerNotFound' key={notFoundIdx}>
               <h1>404 - Not Found</h1>
               <h1>
-                <Link to={`/drinks-details/${notFoundMap.idDrink}`} > Try this random Cocktail!
+                <Link className='randomLink' to={`/drinks-details/${notFoundMap.idDrink}`} > Try this Cocktail suggestion!
                 </Link>
               </h1>
+              <img className='notFoundImage' src='https://imgur.com/jFROHy1.jpg'/>
             </div>
                 )
         })
